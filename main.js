62bb8ce35a9610408452be38517eb6bfba970b6f
lautili <lautilizardi@gmail.com>
2025-12-28 01:03:47 -0300

    Primer Commit

---
+++ main.js
@@ -0,0 +1,244 @@
+let CARTAS = [
+    {
+        img:"https://pbs.twimg.com/profile_images/2002115726459416577/qb1i7P5-_400x400.jpg",
+        nombre:"Wapu"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/1993810455127560192/ADIEHoGw_400x400.jpg",
+        nombre:"ElKuky"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/2003708637856808960/sEJnkxAv_400x400.jpg",
+        nombre:"Mar"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/1995394553612787712/7NFj_qT5_400x400.jpg",
+        nombre:"Manuin"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/1995349409693921280/PpgplceF_400x400.jpg",
+        nombre:"Juli Twt"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/2003316045495304192/BWROo_dl_400x400.jpg",
+        nombre:"Laga"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/2004799927088472064/Yafida22_400x400.jpg",
+        nombre:"Lucas"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/2004729723318845440/YGCjejOe_400x400.jpg",
+        nombre:"Rosa"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/1996793736173346816/tg7YLCRN_400x400.jpg",
+        nombre:"Spa"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/1988304186191044608/j6OP-ntZ_400x400.jpg",
+        nombre:"Yurilover"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/1943027854129700864/PttYw1dV_400x400.jpg",
+        nombre:"Nuzo"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/1994475530918223873/snHWlk86_400x400.png",
+        nombre:"Ashley"
+    },
+    {
+        img:"https://i.pinimg.com/736x/33/70/36/337036424915bb651dfdd11410251f8e.jpg",
+        nombre:"Sunny"
+    },
+    {
+        img:"https://i.pinimg.com/736x/c1/98/04/c19804eb6026c96094c6891b3abb9d5e.jpg",
+        nombre:"Aubrey"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/1747081562611474432/YlX3VnJh_400x400.jpg",
+        nombre:"Basil"
+    },
+    {
+        img:"https://i.pinimg.com/736x/18/ce/5b/18ce5b42018d6cee733e336a5e3e7e4e.jpg",
+        nombre:"Kel"
+    },
+    {
+        img:"https://avatarfiles.alphacoders.com/335/thumb-1920-335722.jpg",
+        nombre:"Hero"
+    },
+    {
+        img:"https://preview.redd.it/does-anyone-else-think-that-mari-v0-3cwobfow97cc1.jpeg?auto=webp&s=ac9fb11c74d1f359813bc5138cc1bbd363ff2bf5",
+        nombre:"Mari"
+    },
+    {
+        img:"https://pbs.twimg.com/media/Es6jE4hWMAIXxeA.jpg",
+        nombre:"Omori"
+    },
+    {
+        img:"https://ih1.redbubble.net/image.3948286115.7099/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.webp",
+        nombre:"Pessi"
+    },
+    {
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRh3PVhkVWdKrlRQRTvN0DtAAV53FAxLLUSGw&s",
+        nombre:"Freddy Fazbear"
+    },
+    {
+        img:"https://media.discordapp.net/attachments/1418016924683735092/1454665686453457077/AIdro_lHR4vbBQczZbGBY_QdRkkkHB7F6YqwfENnxh2cZfPeZQs176-c-k-c0x00ffffff-no-rj-mo.png?ex=6951ea7e&is=695098fe&hm=b846569b6b1cf9795d9d92d12ae2a9789810bd3b5467fe7fc76df141781cc2c1&=&format=webp&quality=lossless&width=220&height=220",
+        nombre:"Nancy the Fox"
+    },
+    {
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlDm2mC0VElddbCUV0IoOdOTAtU9NwdLZxSw&s",
+        nombre:"Flamethrower Bare Endo"
+    },
+    {
+        img:"https://static.wikia.nocookie.net/fnafapedia/images/b/bc/855.png",
+        nombre:"Springtrap"
+    },
+    {
+        img:"https://i.pinimg.com/736x/97/2a/d5/972ad5e139dff32aa58da1020fb62509.jpg",
+        nombre:"Foxy"
+    },
+    {
+        img:"https://i.pinimg.com/736x/57/31/7f/57317f8123336a4965817b7e03b9543c.jpg",
+        nombre:"Funtime Freddy"
+    },
+    {
+        img:"https://i.pinimg.com/736x/de/e6/a3/dee6a3e67bef3f1be2c7c3e03f78ec47.jpg",
+        nombre:"Maxyne Caulfield"
+    },
+    {
+        img:"https://i.pinimg.com/1200x/ea/12/aa/ea12aab97b7b8be8b542e5662824a215.jpg",
+        nombre:"Chloe Price"
+    },
+    {
+        img:"https://i.redd.it/jmxfnmz6g3d91.jpg",
+        nombre:"Fraudchel Amber"
+    },
+    {
+        img:"https://cdn.lifeisstrangefans.com/uploads/media/2020/06/Samuel-Taylor-Avatar.jpg",
+        nombre:"Samuel"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/781485245908471808/tHmCEnHn_400x400.jpg",
+        nombre:"Warren"
+    },
+    {
+        img:"https://cdn.lifeisstrangefans.com/uploads/media/2020/02/William-Price-Avatar.jpg",
+        nombre:"William"
+    },
+    {
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm_mDVG4MfJj4hYP1eR-wcUPrJxBxvqdpzGQ&s",
+        nombre:"Luz"
+    },
+    {
+        img:"https://i.pinimg.com/736x/ce/7f/da/ce7fda2ac467ec3f6dd5e8034bcd986f.jpg",
+        nombre:"Eda"
+    },
+    {
+        img:"https://64.media.tumblr.com/71144a91c7eb017b35bb0fbc2a04fd4d/6e00aa3c5bd35250-d3/s1280x1920/fb2bf304aac5ddd50cfe0040c9bfc8a27ffe66c7.pnj",
+        nombre:"King"
+    },
+    {
+        img:"https://i.pinimg.com/736x/e3/c7/77/e3c777b165fc3df74e47234f145fbf69.jpg",
+        nombre:"Amity"
+    },
+    {
+        img:"https://image.api.playstation.com/cdn/EP2149/CUSA11650_00/wvKCtS1ImtX6YJL3xTR67g4YI5F6DXGF.png?w=240&thumb=false",
+        nombre:"Cazador"
+    },
+    {
+        img:"https://static.wikia.nocookie.net/slimerancher/images/3/39/Slime-MOSAIC_SLIME-Awe_03-11-2023_18-23-49.106.png",
+        nombre:"Mosaico"
+    },
+    {
+        img:"https://ih1.redbubble.net/image.5509716237.6651/st,small,507x507-pad,600x600,f8f8f8.jpg",
+        nombre:"Charco"
+    },
+    {
+        img:"https://i.redd.it/6rypkdo3ge681.jpg",
+        nombre:"Spamton"
+    },
+    {
+        img:"https://i.pinimg.com/736x/f8/a9/1a/f8a91a9532439993dcbfe9dcaf3ed862.jpg",
+        nombre:"Kris"
+    },
+    {
+        img:"https://i.pinimg.com/736x/82/92/ac/8292acb4aa334ac90fbac595321534e0.jpg",
+        nombre:"Susie"
+    },
+    {
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8F8wTvnlih0jf3aGkcYcTuvmztPKnHxLVlQ&s",
+        nombre:"Ralsei"
+    },
+    {
+        img:"https://i.pinimg.com/474x/08/21/1f/08211fc874de6a5cf46c3fda7bd07db3.jpg",
+        nombre:"Tenna"
+    }
+]
+let state = 0;
+let select = false;
+createCards();
+function createCards(){
+    let board = document.querySelector("#Cartas");
+    for (let card of CARTAS){
+        let div = document.createElement('div');
+        let pfp = document.createElement('img');
+        let name = document.createElement('h2');
+        let {img, nombre} = card;
+        pfp.src=img;
+        name.textContent=nombre;
+        div.classList.add("card_normal");
+        div.append(pfp, name);
+        div.addEventListener("click", handleClick);
+        board.append(div);
+    }
+}
+
+function handleClick(e){
+    let div = e.currentTarget;
+    if(state){
+        if (div.classList.contains("card_normal")){
+        div.classList.remove("card_normal");
+        div.classList.add("card_black");
+        }else {
+        div.classList.remove("card_black");
+        div.classList.add("card_normal");
+        }
+    }else{
+        let sel  = document.querySelector(".blackselected");
+        let tarimg = sel.children[0];
+        let tarname = sel.children[1];
+        let img  = div.children[0];
+        let name = div.children[1];
+        tarimg.src = img.src;
+        tarname.textContent = name.textContent;
+        select=true;
+    }
+}
+function handleConfirm(){
+    let sel = document.querySelector("#selected");
+    if(select==true){
+        sel.classList.add("selected");
+        sel.classList.remove("blackselected");
+    }
+    state=1;
+}
+function handleReset(){
+    let divs = document.querySelectorAll(".card_black");
+    for (let div of divs){
+        if(div.classList.contains("card_black")){
+            div.classList.remove("card_black");
+            div.classList.add("card_normal");
+        }
+    }
+    let sel = document.querySelector("#selected");
+    if (sel.classList.contains("selected")){
+        sel.classList.add("blackselected");
+        sel.classList.remove("selected");
+    }
+    sel.children[0].src = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Icon-round-Question_mark.svg/2048px-Icon-round-Question_mark.svg.png";
+    sel.children[1].textContent = "No Seleccionado";
+    state=0;
+    select=false;
+}
