ff8d5d59b7aa2a0b5402274e754c9e3c5f6969f3
lautili <lautilizardi@gmail.com>
2025-12-28 23:45:01 -0300

    Noveno Commit

--- main.js
+++ main.js
@@ -4,16 +4,20 @@ let CARTAS = [
         nombre:"Chiqui Mafia"
     },
     {
-        img:"https://pbs.twimg.com/profile_images/2002115726459416577/qb1i7P5-_400x400.jpg",
-        nombre:"Wapu"
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Official_Presidential_Portrait_of_President_Donald_J._Trump_%282025%29.jpg/250px-Official_Presidential_Portrait_of_President_Donald_J._Trump_%282025%29.jpg",
+        nombre:"Donald Trump"
     },
     {
-        img:"https://pbs.twimg.com/profile_images/1993810455127560192/ADIEHoGw_400x400.jpg",
-        nombre:"ElKuky"
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3CGtwwMdjrLhNfhVoZd1fml2RSinZ1HGzlg&s",
+        nombre:"Elon Musk"
     },
     {
-        img:"https://pbs.twimg.com/profile_images/2003708637856808960/sEJnkxAv_400x400.jpg",
-        nombre:"Mar"
+        img:"https://chequeado.com/wp-content/uploads/2023/11/Javier-Milei-Perfil-TW.jpeg",
+        nombre:"Javier Milei"
+    },
+    {
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK1GRjxxLbSiBzkWPn-eaH6csEfVHGv0vgzQ&s",
+        nombre:"Juan D. Perón"
     },
     {
         img:"https://pbs.twimg.com/profile_images/1995394553612787712/7NFj_qT5_400x400.jpg",
@@ -26,6 +30,18 @@ let CARTAS = [
     {
         img:"https://pbs.twimg.com/profile_images/2003316045495304192/BWROo_dl_400x400.jpg",
         nombre:"Laga"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/2002115726459416577/qb1i7P5-_400x400.jpg",
+        nombre:"Wapu"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/1993810455127560192/ADIEHoGw_400x400.jpg",
+        nombre:"ElKuky"
+    },
+    {
+        img:"https://pbs.twimg.com/profile_images/2003708637856808960/sEJnkxAv_400x400.jpg",
+        nombre:"Mar"
     },
     {
         img:"https://pbs.twimg.com/profile_images/2004799927088472064/Yafida22_400x400.jpg",
@@ -80,6 +96,46 @@ let CARTAS = [
         nombre:"Omori"
     },
     {
+        img:"https://i.redd.it/udwyd92dbk3a1.jpg",
+        nombre:"Kim"
+    },
+    {
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSihggKXicMbs9gOkS1WT6BLjpbGyqK9wo7fQ&s",
+        nombre:"Sweetheart"
+    },
+    {
+        img:"https://i.pinimg.com/736x/8f/35/66/8f3566b66a7153f4207b8714d96e3d23.jpg",
+        nombre:"Captain Spaceboy"
+    },
+    {
+        img:"https://i.redd.it/yj6t95xtsaab1.png",
+        nombre:"Jawsum"
+    },
+    {
+        img:"https://ih1.redbubble.net/image.3134947502.2467/raf,360x360,075,t,fafafa:ca443f4786.jpg",
+        nombre:"SOMETHING"
+    },
+    {
+        img:"https://art.ngfiles.com/thumbnails/1645000/1645188_full.png?f1613420106",
+        nombre:"Roboheart"
+    },
+    {
+        img:"https://i.pinimg.com/736x/b6/aa/94/b6aa949a208e944a41bcfe4f3332a2b1.jpg",
+        nombre:"Mutantheart"
+    },
+    {
+        img:"https://media.tenor.com/TUnNfAiKV_0AAAAe/perfectheart-omori.png",
+        nombre:"Perfectheart"
+    },
+    {
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReqiMp-tUlBUlDtsyz8vXsm7csvJUDUGYe6Q&s",
+        nombre:"Pluto"
+    },
+    {
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHgd3RXeFFwp27AkB6jMoVAAzZKrvolKEcyw&s",
+        nombre:"OMOCAT"
+    },
+    {
         img:"https://ih1.redbubble.net/image.3948286115.7099/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.webp",
         nombre:"Pessi"
     },
@@ -108,8 +164,64 @@ let CARTAS = [
         nombre:"Funtime Freddy"
     },
     {
+        img:"https://i.redd.it/5i0p5ezikhib1.png",
+        nombre:"Puppet"
+    },
+    {
+        img:"https://i.redd.it/27s06h2fohy01.jpg",
+        nombre:"Baby"
+    },
+    {
+        img:"https://i.pinimg.com/736x/6f/fa/21/6ffa215759ecc80c04f035116c7c8a0a.jpg",
+        nombre:"Lefty"
+    },
+    {
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0z8IQYN_iWc4TehdiAXr_XRRAGzZvPZzOEg&s",
+        nombre:"Dark Water Bare Endo"
+    },
+    {
+        img:"https://i.pinimg.com/736x/fb/97/76/fb9776460a72d94efba776b56dc82776.jpg",
+        nombre:"Funtime Foxy"
+    },
+    {
+        img:"https://static.wikia.nocookie.net/heroes-villains/images/8/83/FNAF_Mangle.png",
+        nombre:"Mangle"
+    },
+    {
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs-eZOmotg4LKYiiBZ2LZ2jwIzWsDouxXd2w&s",
+        nombre:"Golden Freddy"
+    },
+    {
+        img:"https://i.pinimg.com/736x/2b/5c/7f/2b5c7ffd4e22982547f464f78abb3694.jpg",
+        nombre:"Ennard"
+    },
+    {
+        img:"https://preview.redd.it/in-your-opinion-is-william-afton-a-good-villain-character-v0-87d7g071eq2b1.jpg?width=1080&crop=smart&auto=webp&s=496f19beeeba012f2353864eaa86efe9875c1322",
+        nombre:"William Afton"
+    },
+    {
+        img:"https://media.tenor.com/WPixCTaI-7kAAAAe/tranparent-michael-afton.png",
+        nombre:"Michael Afton"
+    },
+    {
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSddXxqgmtpLo7LchGfNvg1giY4VCoDIoynXg&s",
+        nombre:"Elizabeth Afton"
+    },
+    {
+        img:"https://i.redd.it/macwqxz5173f1.png",
+        nombre:"Crying Child"
+    },
+    {
+        img:"https://i.redd.it/rnk0ecncz8la1.jpg",
+        nombre:"Henry Emily"
+    },
+    {
+        img:"https://media.discordapp.net/attachments/1418016924683735092/1455024210937385004/m98holnuifu51.png?ex=69533864&is=6951e6e4&hm=c59d2e1780ea386396e31a2d691033c929542565ad9ef564789fcc555e36c3b4&=&format=webp&quality=lossless&width=800&height=791",
+        nombre:"Scott Cawthon"
+    },
+    {
         img:"https://i.pinimg.com/736x/de/e6/a3/dee6a3e67bef3f1be2c7c3e03f78ec47.jpg",
-        nombre:"Maxyne Caulfield"
+        nombre:"Maxine Caulfield"
     },
     {
         img:"https://i.pinimg.com/1200x/ea/12/aa/ea12aab97b7b8be8b542e5662824a215.jpg",
@@ -164,6 +276,18 @@ let CARTAS = [
         nombre:"Spamton"
     },
     {
+        img:"https://i.redd.it/flhbv6aeuaz71.jpg",
+        nombre:"Jevil"
+    },
+    {
+        img:"https://preview.redd.it/who-is-the-roaring-knight-a-theory-v0-af6u2uglrj5f1.png?width=302&format=png&auto=webp&s=9f33577705ce023d0250f5810c5312d78b8436a5",
+        nombre:"Roaring Knight"
+    },
+    {
+        img:"https://media.tenor.com/HgsokQEYVX0AAAAe/gerson-boom-gerson.png",
+        nombre:"Gerson"
+    },
+    {
         img:"https://i.pinimg.com/736x/f8/a9/1a/f8a91a9532439993dcbfe9dcaf3ed862.jpg",
         nombre:"Kris"
     },
@@ -178,6 +302,74 @@ let CARTAS = [
     {
         img:"https://i.pinimg.com/474x/08/21/1f/08211fc874de6a5cf46c3fda7bd07db3.jpg",
         nombre:"Tenna"
+    },
+    {
+        img:"https://i.pinimg.com/736x/8d/d7/ba/8dd7bafc329295768846da5304fd74ed.jpg",
+        nombre:"Noelle"
+    },
+    {
+        img:"https://media.tenor.com/ETlaWvH-ieQAAAAe/dess-holiday-deltarune.png",
+        nombre:"Dess"
+    },
+    {
+        img:"https://media.tenor.com/ETlaWvH-ieQAAAAe/dess-holiday-deltarune.png",
+        nombre:"Chester"
+    },
+    {
+        img:"https://ih1.redbubble.net/image.4523286838.5423/flat,750x,075,f-pad,750x1000,f8f8f8.jpg",
+        nombre:"Original Starwalker"
+    },
+    {
+        img:"https://preview.redd.it/petition-to-make-moai-lancer-the-new-icon-of-the-sub-v0-7uo0triqr2nb1.jpeg?width=828&format=pjpg&auto=webp&s=411e073ec9d3c3353decc59fa74a102605fea6aa",
+        nombre:"Lancer"
+    },
+    {
+        img:"https://i.redd.it/yuxqnwn13ruf1.jpeg",
+        nombre:"Asgore"
+    },
+    {
+        img:"https://i.pinimg.com/736x/f2/38/e2/f238e203bcb28007b4c278b5c35bfdba.jpg",
+        nombre:"Toriel"
+    },
+    {
+        img:"https://static.wikia.nocookie.net/liberproeliis/images/c/c5/Asriel_Dreemurr.jpg",
+        nombre:"Asriel"
+    },
+    {
+        img:"https://static.wikia.nocookie.net/undertale/images/c/c4/Undyne_face.png",
+        nombre:"Undyne"
+    },
+    {
+        img:"https://i.pinimg.com/736x/8d/df/23/8ddf23eb80486c322ebb0bad7a11e159.jpg",
+        nombre:"Mettaton"
+    },
+    {
+        img:"https://external-preview.redd.it/Lg9NZJii_EtHF-vra0EjZcD3DzNsp1CFr7ZFeL-2Osk.jpg?width=640&crop=smart&auto=webp&s=1b3f862bacc18c76908feaef99667280e5ee8c80",
+        nombre:"Sans"
+    },
+    {
+        img:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/c7fa16ea-4589-41f1-98d6-dff7bbdbbaba/d9hk6gm-a95cce7d-0835-43b9-a9f5-15a78529aca2.jpg",
+        nombre:"Papyrus"
+    },
+    {
+        img:"https://i.pinimg.com/736x/d9/dd/8e/d9dd8ef9c953e211a61101cf59289027.jpg",
+        nombre:"Gaster"
+    },
+    {
+        img:"https://media.discordapp.net/attachments/784301091369385997/1455027901237035060/image.png?ex=69533bd4&is=6951ea54&hm=06cece917885fee7943fa1fb7407d786fc7a0c39c655c3d7904eee1d001a5d6b&=&format=webp&quality=lossless&width=269&height=233",
+        nombre:"Frisk"
+    },
+    {
+        img:"https://i.pinimg.com/736x/90/c7/29/90c7298b06a674f4791a8684ed24a922.jpg",
+        nombre:"Chara"
+    },
+    {
+        img:"https://preview.redd.it/clover-appreciation-thread-v0-92ouo3ypxnac1.jpg?width=416&format=pjpg&auto=webp&s=c3cdab9cd0d78ebd493f7872157bb7107526576a",
+        nombre:"Clover"
+    },
+    {
+        img:"https://i.redd.it/am-i-the-only-one-who-feels-like-every-popular-image-of-v0-lm071n302dqe1.jpg?width=500&format=pjpg&auto=webp&s=e888d68c1c8dc6d20002b350945822d29a4dbb2b",
+        nombre:"Toby Fox"
     }
 ]
 let state = 0;
